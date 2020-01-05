579ec2fc745ad43e7b80e10c173f33d713ff0d7e
Andreas Lüdemann <andreaslydemann@gmail.com>
2020-01-05 19:44:42 +0100

    Initial commit

---
+++ TodoApp/SceneDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        defer { self.window = window }
+
+        window.windowScene = windowScene
+
+        window.rootViewController = UINavigationController(rootViewController: CategoryViewController())
+        window.makeKeyAndVisible()
+    }
+}
